7eda77ab58bc3979f747038919ea7988973dd8f7
jc <juanchengrong@gmail.com>
2018-03-16 07:24:20 +0800

    added in fabric framework

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-
+import { initializeIcons } from '@uifabric/icons';
 import { renderRoutes } from '../imports/startup/client/routes.js';
  
 Meteor.startup(() => {
+  initializeIcons();
   render(renderRoutes(), document.getElementById('app'));
 });
